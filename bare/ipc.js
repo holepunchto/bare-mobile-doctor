07dc9a80ddfc1716999c57d04621de5eab034102
Tony Gorez <gorez.tony@gmail.com>
2025-03-19 22:54:09 +0100

    feat(ipc): split in crypto, native and basic
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- bare/ipc.js
+++ bare/ipc.js
@@ -1,30 +1,40 @@
 const sodium = require('sodium-native')
+const { get, set, findFirst } = require('quickbit-native')
 
 console.log('Worklet started')
 
+function doCryptoWork(iterations) {
+  let acc = ''
+
+  for (let i = 0; i < iterations; i++) {
+    const buf = Buffer.from('Hello, World!')
+    const out = Buffer.alloc(sodium.crypto_generichash_BYTES)
+    sodium.crypto_generichash(out, buf)
+    acc += out.toString('hex').slice(0, 2)
+  }
+
+  return acc
+}
+
+function basicWork() {
+  return 42;
+}
+
+function doNativeWork(iterations) {
+  for (let i = 0; i < iterations; i++) {
+    const field = Buffer.alloc(256)
+    set(field, 1000)
+  }
+}
+
 BareKit.IPC.on('data', (data) => {
-  function heavyMathLoad(iterations) {
-    let acc = ''
-
-    for (let i = 0; i < iterations; i++) {
-      const buf = Buffer.from('Hello, World!')
-      const out = Buffer.alloc(sodium.crypto_generichash_BYTES)
-      sodium.crypto_generichash(out, buf)
-      acc += out.toString('hex').slice(0, 2)
-    }
-
-    return acc
-  }
-
-  function basicWork() {
-    return 42;
-  }
-
   const messages = data.toString().split('-').filter(Boolean)
   messages.forEach((message) => {
     const payload = JSON.parse(message);
-    if (payload.workType === "intensive") {
-      heavyMathLoad(100_000)
+    if (payload.workType === "crypto") {
+      doCryptoWork(100_000)
+    } if (payload.workType === "native") {
+      doNativeWork(100_000)
     } else {
       basicWork()
     }
