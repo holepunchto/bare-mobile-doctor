443665c872ef1a1cfc5ed46e12263020563aa1bd
supersuryaansh <prod.genx@gmail.com>
2025-03-21 20:45:46 +0530

    Add Hyperbee test

--- bare/hyperdb/hyperdb.mjs
+++ bare/hyperdb/hyperdb.mjs
@@ -1,4 +1,6 @@
 import HyperDB from 'hyperdb'
+import Hyperswarm from 'hyperswarm'
+import Corestore from 'corestore'
 import db from './spec/db/index.js'
 
 console.log('Hyperdb Worklet started')
@@ -23,6 +25,69 @@ BareKit.IPC.on('data', async (data) => {
       await local.flush() // Persist changes
       id++
     }
+  } else if (payload.workType == 'hyperbee') {
+    const topic = Buffer.alloc(32).fill('codeispoetry')
+    const localCorestore = new Corestore(path + '/local.db')
+
+    // returns a hypercore
+    const a = localCorestore.get({ name: 'local' })
+
+    const local = HyperDB.bee(a, db, { autoUpdate: true })
+    await local.ready()
+    const bootstrapKey = local.core.key // we will bootstrap peer core from this key
+    for (let i = 0; i < payload.recordsAmount; i++) {
+      const timestamp = Math.floor(Date.now() / 1000)
+      await local.insert('@hyperdb-example/user', {
+        id: i + 1,
+        name: `data-${i + 1}-${timestamp}`
+      })
+      await local.flush() // Persist changes
+      id++
+    }
+
+    // Create a new hyperswarm
+    const localSwarm = new Hyperswarm()
+
+    localSwarm.on('connection', async (conn) => {
+      localCorestore.replicate(conn) // replicate local core across all peers
+    })
+
+    const swarm1 = localSwarm.join(topic)
+    await swarm1.flushed() // finish joining the swarm
+
+    // Storage for second peer
+    const remoteCorestore = new Corestore(path + '/remote.db')
+    // Use the first peer's bootstrap key to create this hypercore
+    const b = remoteCorestore.get({ key: bootstrapKey })
+
+    const remote = HyperDB.bee(b, db, { autoUpdate: true })
+    await remote.ready()
+
+    const remoteSwarm = new Hyperswarm()
+    remoteSwarm.on('connection', async (conn) => {
+      console.log('Received a connection')
+      remoteCorestore.replicate(conn)
+    })
+
+    const swarm2 = remoteSwarm.join(topic)
+    await swarm2.flushed()
+
+    // A watcher can watch when the database has updated
+    const watcher = remote.db.watch()
+    watcher.on('update', async () => {
+      let result = await local.find(
+        '@hyperdb-example/user',
+        { reverse: true },
+        { limit: 1 }
+      ) // list only one due to limit
+      result = await result.toArray()
+
+      if (result.id === payload.recordsAmount) {
+        BareKit.IPC.write(JSON.stringify(result))
+        await local.close() // close the db
+        await remote.close()
+      }
+    })
   } else {
     for (let i = 0; i < payload.recordsAmount; i++) {
       const timestamp = Math.floor(Date.now() / 1000)
@@ -34,6 +99,7 @@ BareKit.IPC.on('data', async (data) => {
     }
     await local.flush() // Flush only once after writing all the records
   }
+
   let result = await local.find(
     '@hyperdb-example/user',
     { reverse: true },
@@ -43,6 +109,5 @@ BareKit.IPC.on('data', async (data) => {
   await local.close() // close the db
   BareKit.IPC.write(JSON.stringify(result))
 })
-
 
 //run and debug in console
