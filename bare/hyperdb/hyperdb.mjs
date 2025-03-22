91be79cfcb93986cda5ec020aa0d9bdec5c18730
supersuryaansh <prod.genx@gmail.com>
2025-03-22 09:47:38 +0530

    Clean and prepare for adding more bee tests

--- bare/hyperdb/hyperdb.mjs
+++ bare/hyperdb/hyperdb.mjs
@@ -4,6 +4,11 @@ import Corestore from 'corestore'
 import db from './spec/db/index.js'
 
 console.log('Hyperdb Worklet started')
+
+function time() {
+  return Math.floor(Date.now() / 1000)
+}
+
 const path =
   Bare.argv[0] === 'android'
     ? '/data/data/to.holepunch.bare.doctor/bare-mobile-doctor'
@@ -15,31 +20,30 @@ BareKit.IPC.on('data', async (data) => {
 
   const message = data.toString()
   const payload = JSON.parse(message)
+
   if (payload.workType === 'intensive') {
     for (let i = 0; i < payload.recordsAmount; i++) {
-      const timestamp = Math.floor(Date.now() / 1000)
       await local.insert('@hyperdb-example/user', {
         id: i + 1,
-        name: `data-${i + 1}-${timestamp}`
+        name: `data-${i + 1}-${time()}`
       })
       await local.flush() // Persist changes
       id++
     }
-  } else if (payload.workType == 'hyperbee') {
-    const topic = Buffer.alloc(32).fill('codeispoetry')
-    const localCorestore = new Corestore(path + '/local.db')
+  } else if (payload.workType === 'hyperbee') {
 
-    // returns a hypercore
+    const topic = Buffer.alloc(32).fill('codeispoetry')
+    const localCorestore = new Corestore(path + `/${time()}/local.db`)
+
     const a = localCorestore.get({ name: 'local' })
 
     const local = HyperDB.bee(a, db, { autoUpdate: true })
     await local.ready()
     const bootstrapKey = local.core.key // we will bootstrap peer core from this key
     for (let i = 0; i < payload.recordsAmount; i++) {
-      const timestamp = Math.floor(Date.now() / 1000)
       await local.insert('@hyperdb-example/user', {
         id: i + 1,
-        name: `data-${i + 1}-${timestamp}`
+        name: `data-${i + 1}-${time()}`
       })
       await local.flush() // Persist changes
       id++
@@ -55,7 +59,7 @@ BareKit.IPC.on('data', async (data) => {
     const swarm1 = localSwarm.join(topic)
 
     // Storage for second peer
-    const remoteCorestore = new Corestore(path + '/remote.db')
+    const remoteCorestore = new Corestore(path + `/${time()}/local.db`)
     // Use the first peer's bootstrap key to create this hypercore
     const b = remoteCorestore.get({ key: bootstrapKey })
 
@@ -70,28 +74,27 @@ BareKit.IPC.on('data', async (data) => {
 
     const swarm2 = remoteSwarm.join(topic)
 
-    // A watcher can watch when the database has updated
+    // Watch for db updates
     const watcher = local.db.watch()
     watcher.on('update', async () => {
       let result = await local.find(
         '@hyperdb-example/user',
         { reverse: true },
         { limit: 1 }
-      ) // list only one due to limit
+      )
       result = await result.toArray()
 
       if (result.id === payload.recordsAmount) {
+        await localCorestore.close()
+        await remoteCorestore.close()
         BareKit.IPC.write(JSON.stringify(result))
-        await local.close() // close the db
-        await remote.close()
       }
     })
-  } else {
+  } else if (payload.workType === 'basic') {
     for (let i = 0; i < payload.recordsAmount; i++) {
-      const timestamp = Math.floor(Date.now() / 1000)
       await local.insert('@hyperdb-example/user', {
         id: i + 1,
-        name: `data-${i + 1}-${timestamp}`
+        name: `data-${i + 1}-${time()}`
       })
       id++
     }
@@ -106,6 +109,7 @@ BareKit.IPC.on('data', async (data) => {
   result = await result.toArray()
   await local.close() // close the db
   BareKit.IPC.write(JSON.stringify(result))
+
 })
 
 //run and debug in console
