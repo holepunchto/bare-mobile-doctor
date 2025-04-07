1a57e69ede8b855a15637c7c616e5f583bbae161
Tony Gorez <gorez.tony@gmail.com>
2025-04-07 12:03:47 +0100

    feat(os): add interval (#22)
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- bare/os.js
+++ bare/os.js
@@ -1,11 +1,15 @@
 const ptop = require('process-top')
 
+let timer = null
 BareKit.IPC.setEncoding('utf8')
 BareKit.IPC.on('data', function(data) {
+  clearInterval(timer)
   let message = JSON.parse(data)
   if (message.type) {
-    const top = ptop()
-    const res = message.type === 'cpu' ? top.cpu() : top.memory()
-    BareKit.IPC.write(JSON.stringify(res))
+    timer = setInterval(() => {
+      const top = ptop()
+      const res = message.type === 'cpu' ? top.cpu() : top.memory()
+      BareKit.IPC.write(JSON.stringify(res))
+    }, 1000)
   }
 })
